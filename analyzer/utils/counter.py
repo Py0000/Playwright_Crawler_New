3f6d6830c37c6752abd7a8ea9c41e2890c89cf7c
Py0000 <pylim8266@gmail.com>
2024-02-13 16:27:40 +0800

    update counter script

--- analyzer/utils/counter.py
+++ analyzer/utils/counter.py
@@ -5,6 +5,7 @@ from analyzer.utils import file_utils
 
 class Counter:
     def __init__(self, type):
+        self.accumulated = 0
         self.type = type
         self.count_txt_file = os.path.join("analyzer", "utils", f"dataset_count_{self.type}.txt")
     
@@ -23,14 +24,22 @@ class Counter:
             folder_path = os.path.join(type_path, subfolder)
             files = os.listdir(folder_path)
             zip_counts[subfolder] = sum(1 for file in files if file.endswith(".zip"))
-        
+
+        total_count = sum(zip_counts.values())
+        zip_counts["Total"] = total_count
+        self.accumulated = self.accumulated + total_count
+
         print(date, zip_counts)
         with open(self.count_txt_file, 'a') as file:
             file.write(f"{str(date)}\n")
             for subfolder, count in zip_counts.items():
                 file.write(f"Number of ZIP files in {subfolder}: {count}\n")
             file.write("\n")
-
+        
+    def save_accumulated(self):
+        print(self.accumulated)
+        with open(self.count_txt_file, 'a') as file:
+            file.write(f"Accumulated: {self.accumulated}")
     
 
 if __name__ == '__main__':
@@ -50,5 +59,6 @@ if __name__ == '__main__':
             date = re.search(r"dataset_(\d+)", folder).group(1)
             counter.get_count_of_one_day_dataset(os.path.join(month_folder_path, folder), date)
     
-   
+    counter.save_accumulated()
+
 
